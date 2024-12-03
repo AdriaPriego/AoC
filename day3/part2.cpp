7d600ac33c4833451d15f4eeca35cd9e153e0cfd
AdriaPriego <adripg12@gmail.com>
2024-12-03 11:48:05 +0100

    Day 3

--- day3/part2.cpp
+++ day3/part2.cpp
@@ -24,18 +24,17 @@ int main() {
     std::smatch matchdo;
     std::smatch matchdont;
 
-    while (search_start != corrupted_memory.cend()) {
-        if(!std::regex_search(search_start, corrupted_memory.cend(), match, valid_mul_pattern) &&
-        !std::regex_search(search_start, corrupted_memory.cend(), matchdo, do_pattern) &&
-        !std::regex_search(search_start, corrupted_memory.cend(), matchdont, dont_pattern))
-            break;
-        if (match.position(0) < matchdo.position(0) && match.position(0) < matchdont.position(0)) {
+    while (std::regex_search(search_start, corrupted_memory.cend(), match, valid_mul_pattern)) {
+        std::regex_search(search_start, corrupted_memory.cend(), match, valid_mul_pattern);
+        std::regex_search(search_start, corrupted_memory.cend(), matchdo, do_pattern);
+        std::regex_search(search_start, corrupted_memory.cend(), matchdont, dont_pattern);
+        if ((matchdo.empty() || match.position(0) < matchdo.position(0)) && (matchdont.empty() || match.position(0) < matchdont.position(0))) {
             if (mul_enabled) {
                 int x = std::stoi(match[1].str());
                 int y = std::stoi(match[2].str());
                 total_sum += x * y;
-                search_start = match.suffix().first;
             }
+            search_start = match.suffix().first;
         } else if (matchdo.position(0) < matchdont.position(0)) {
             mul_enabled = true;
             search_start = matchdo.suffix().first;
@@ -43,7 +42,6 @@ int main() {
             mul_enabled = false;
             search_start = matchdont.suffix().first;
         }
-
     }
     std::cout << total_sum << std::endl;
 
